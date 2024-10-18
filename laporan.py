bdf2c741b2f4938e85f532d8649e872ab726404f
raugarhan131 <raufarhan131@gmail.com>
2024-10-19 04:57:57 +0700

    Update laporan.py

--- laporan.py
+++ laporan.py
@@ -1,7 +1,7 @@
 
 import os
-import tkinter as tk
-from tkinter import filedialog, ttk, messagebox
+#import tkinter as tk
+#from tkinter import filedialog, ttk, messagebox
 import pandas as pd
 import streamlit as st
 import sqlite3
